ea6c41c1254d67f4b4bd79c4d600055abaed87a4
k-naito <projecthonwaka@gmail.com>
2025-08-16 16:13:38 +0900

    ファクタリング_カスタマイズ性を高める(表示可否,任意メッセージ)

--- src/components/common/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner.tsx
@@ -1,13 +1,17 @@
 type LoadingSpinnerProps = {
-  loading: boolean
+  loading?: boolean   // @memo 表示、非表示をカスタマイズしやすくしている。
+  message?: string    // @memo カスタムメッセージに変更することも可能。
 }
 
 /** ローディング表示 */
-export const LoadingSpinner: React.FC<LoadingSpinnerProps> = props => {
+export const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({
+  loading = false,
+  message = '📡 為替レートを取得中...'
+}) => {
   return(
-    props.loading && (
+    loading && (
       <div className='loading-message'>
-        📡 為替レートを取得中...
+        {message}
       </div>
     )
   )
